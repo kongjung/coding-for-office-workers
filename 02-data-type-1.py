73e160db701d5f0b3483d379721855cd0f3b7dc3
unknown <dltmf3055@gc.gachon.ac.kr>
2018-05-30 18:21:59 +0900

    Add 02-data-type-1.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,26 @@
+#정수 int
+print(1+1)
+#소수 float
+print(1.5+2.1)
+#문자 str
+print("hello")
+
+#print("hello"+"1")
+#print(4//2)
+#print(int(4/2))
+#print(float(2))
+
+#정수 소수간 변경
+#print(int(4/2))로하면 4//2랑 같이 2만나옴 (2.0안나옴)
+#print(float(2))로하면 2인데 2.0으로 나옴
+
+#문자
+#print("1") 문자
+#print(int("1")) 숫자
+#print(int("1.5")) - error
+#print(int(1.5)) - 1
+
+print("1")
+print(int("1"))
+print(int("1.5"))
+print(int(1.5))
